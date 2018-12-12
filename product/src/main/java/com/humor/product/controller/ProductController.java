b423e8d205575f4c13aee33eda06ee94236dd143
humor <guxingyin@live.cn>
2018-12-12 19:09:37 +0800

    mall商城全新改版

--- product/src/main/java/com/humor/product/controller/ProductController.java
+++ product/src/main/java/com/humor/product/controller/ProductController.java
@@ -3,6 +3,10 @@ package com.humor.product.controller;
 import com.humor.domain.common.ServerResponse;
 import com.humor.domain.entity.Product;
 import com.humor.product.service.IProductService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +17,22 @@ import org.springframework.web.bind.annotation.RestController;
  * @author zhangshaoze
  */
 
+@Api
 @RestController
-@RequestMapping("/product/")
+@RequestMapping("product")
 public class ProductController {
 
     @Autowired
     private IProductService iProductService;
 
+    @ApiOperation(value = "商品列表",httpMethod = "GET",notes = "首页")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "关键字",  value = "商品名称", dataType = "String"),
+            @ApiImplicitParam(name = "分类id",  value = "商品分类id", dataType = "Long"),
+            @ApiImplicitParam(name = "当前页",  value = "默认：0", dataType = "int"),
+            @ApiImplicitParam(name = "每页大小",  value = "默认：15", dataType = "int"),
+            @ApiImplicitParam(name = "排序",  value = "功能待开发", dataType = "String"),
+    })
     @GetMapping("list.do")
     public ServerResponse list(@RequestParam(value = "keyword",required = false)String keyword,
                                          @RequestParam(value = "categoryId",required = false)Long categoryId,
@@ -39,7 +52,5 @@ public class ProductController {
     public ServerResponse<Product> productInfoById(Long productId){
         return iProductService.getProductDetail(productId);
     }
-
-
 
 }
